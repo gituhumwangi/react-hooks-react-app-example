ef1904c41f86509da2aec1f264e7bc48235857d8
dennis <dennis.mwangi@student.moringaschool.com>
2023-07-18 23:05:21 +0300

    Completed assignment

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,18 +1,14 @@
 import React from "react";
 
-// no need to modify anything in here. The final test is looking for the returned JSX from this component
+// no need to modify anything in here. The second test is looking for the returned JSX from this component
 function TestComponent() {
   return (
-    <div className="video">
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/5TbUxGZtwGI"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        title="time video"
-        allowFullScreen
-      ></iframe>
+    <div className="test">
+      <img
+        src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
+        alt="a deeply impressed man, perhaps thinking 'whoa'"
+      />
+      <p>What the Fuck!</p>
     </div>
   );
 }
